9713b7544bcc4b4485d334e9c7761d422f189492
Prerak Shah <prerak9350@gmail.com>
2025-09-15 23:21:12 +0930

    Working with Map, MapGet, MapPut, MapPost, MapDelete

--- ASPCore Empty/Program.cs
+++ ASPCore Empty/Program.cs
@@ -1,8 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
+app.UseRouting();
 
-//app.MapGet("/", () => "Hello World!");
+//app.Map("/Home", () => "Hello World!"); ---> Works on all HTTP Methods
+
+//app.MapGet("/Home", () => "Hello World! --- Get");
+//app.MapPut("/Home", () => "Hello World! --- Put");
+//app.MapPost("/Home", () => "Hello World! ---Post");
+//app.MapDelete("/Home", () => "Hello World! ---Delete");
+
+app.UseEndpoints(endpoints => {
+
+    endpoints.MapGet("/Home", async (context) =>
+    {
+        await context.Response.WriteAsync("Hello to Get Request");
+    });
+
+    endpoints.MapPost("/Home", async (context) =>
+    {
+        await context.Response.WriteAsync("Hello to Post Request");
+    });
+
+    endpoints.MapDelete("/Home", async (context) =>
+    {
+        await context.Response.WriteAsync("Hello to Delete Request");
+    });
+});
+
+app.Run( async(HttpContext context) =>
+{
+    await context.Response.WriteAsync("Hello to the C#");
+});
+
 
 //app.MapGet("/About", () => "About Us Page");
 
@@ -13,6 +43,6 @@ var app = builder.Build();
 //    pattern: "{controller=Home}/{action=About}/{id?}"
 //    );
 
-app.MapControllers();
+//app.MapControllers();
 
 app.Run();
