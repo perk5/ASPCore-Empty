ddec1ba927792cfd77b6219449e6fd8950ccb204
Prerak Shah <prerak9359@gmail.com>
2025-09-15 19:37:18 +0930

    Add project files.

---
+++ ASPCore Empty/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.MapGet("/About", () => "About Us Page");
+
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=About}/{id?}"
+    );
+
+app.Run();
