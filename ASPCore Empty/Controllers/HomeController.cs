76849d6f74536d1972b3b409c804e4f5223442cf
Prerak Shah <prerak9359@gmail.com>
2025-09-15 22:06:33 +0930

    Conventional and attribute based Routing

--- ASPCore Empty/Controllers/HomeController.cs
+++ ASPCore Empty/Controllers/HomeController.cs
@@ -2,19 +2,29 @@
 
 namespace ASPCore_Empty.Controllers
 {
+    [Route("[Controller]/[action]")]
     public class HomeController : Controller
     {
+        [Route("")]
+        [Route("~/")]
+        [Route("~/Home")]
+
+        //[Route("[action]")]
         public IActionResult Index()
         {
             return View();
         }
+
+        //[Route("[action]")]
         public IActionResult About()
         {
             return View();
         }
+
+        [Route("{id?}")]
         public int Details(int id)
         {
-            return id;
+            return id ;
         }
     }
 }
--- ASPCore Empty/Program.cs
+++ ASPCore Empty/Program.cs
@@ -8,9 +8,11 @@ var app = builder.Build();
 
 //app.MapDefaultControllerRoute();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=About}/{id?}"
-    );
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=About}/{id?}"
+//    );
+
+app.MapControllers();
 
 app.Run();
